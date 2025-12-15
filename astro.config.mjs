2f0887a562e75a45b495f6b8958805f745b303b8
Alvaro Hernandez <alvarohernandezassens@gmail.com>
2025-12-15 16:41:52 +0100

    fix route

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  base: isProd ? '/oriol' : '/',
   vite: {
     plugins: [tailwindcss()]
   }
